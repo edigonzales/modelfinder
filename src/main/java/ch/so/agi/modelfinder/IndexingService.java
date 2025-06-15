7e42254e627b119fa0f3bec0c4cc1182e4586747
Stefan Ziegler <edi.gonzales@gmail.com>
2025-06-15 21:04:18 +0200

    wip

---
+++ src/main/java/ch/so/agi/modelfinder/IndexingService.java
@@ -0,0 +1,31 @@
+package ch.so.agi.modelfinder;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndexingService {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final RemoteReaderService readerService;
+    //private final LuceneIndexService indexService;
+    private final IndexingProperties properties;
+    
+    public IndexingService(RemoteReaderService readerService, /*LuceneIndexService indexService,*/
+            IndexingProperties properties) {
+        this.readerService = readerService;
+        //this.indexService = indexService;
+        this.properties = properties;
+    }
+    
+    public void performFullIndex() {
+        properties.repositories().forEach(server -> {
+            Optional<String> data = readerService.fetchData(server);
+            //data.ifPresent(indexService::index);
+        });
+    }
+    
+}
