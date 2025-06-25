01e9f3318cddbf732272bdc2ce0d2299ac9a2689
Stefan Ziegler <edi.gonzales@gmail.com>
2025-06-25 10:57:44 +0200

    try aot cache again with runtime hints

--- src/main/java/ch/so/agi/modelfinder/ResourceRuntimeHints.java
+++ src/main/java/ch/so/agi/modelfinder/ResourceRuntimeHints.java
@@ -20,6 +20,7 @@ public class ResourceRuntimeHints implements RuntimeHintsRegistrar {
             .registerType(JteindexGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
             .registerType(JtemodelmetadataGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
             .registerType(JtemodelsGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
-            .registerType(JtesearchresultsGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);   
+            .registerType(JtesearchresultsGenerated.class, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
+            .registerType(ch.ehi.uml1_4.modelmanagement.Model.class, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.DECLARED_FIELDS);  
     }
 }
